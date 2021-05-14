a6bf63790279cbfb2de65b16407dad8eedbcfd24
Pufferfish101007 <50246616+pufferfish101007@users.noreply.github.com>
2021-05-14 17:21:00 +0100

    Improve speed + make it work
    This works

--- verify-users.js
+++ verify-users.js
@@ -7,7 +7,7 @@ const run = async () => {
   users = await db.get("users");
   console.log(users.length);
   let i = 0;
-  const checkUsr = async (index) => {
+  /*const checkUsr = async (index) => {
     if (index >= users.length) return;
     let res = await fetch("https://api.scratch.mit.edu/users/" + users[index] + "/");
     if (res.status >= 400) {
@@ -26,6 +26,18 @@ const run = async () => {
     //  console.log(users[index] + " ✅");
       if (index % 50 === 0) console.log(index);
       setTimeout(() => checkUsr(index + 1), 108 + Math.floor(Math.random * 30) + (Math.random() > 0.9 ? 50 : 0));
+    }
+  };*/
+  const checkUsr = async index => {
+    let res = await fetch("https://api.scratch.mit.edu/users/" + users[index] + "/");
+    if (res.status >= 400) {
+      console.log(users[index], res.status);
+      users.splice(index, 1);
+      await db.set("users", users);
+      setTimeout(() => checkUsr(index), 101);
+    } else {
+      if (index % 50 === 0) console.log(index, users.length, ((index / 54872) * 100)+"% done");
+      setTimeout(() => checkUsr(index + 1), 113);
     }
   };
   checkUsr(i);
